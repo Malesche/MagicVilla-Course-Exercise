89af12a9b4426176207b5e08d59aa0b880bd1c43
Maria <32207690+Malesche@users.noreply.github.com>
2023-08-04 22:35:45 +0200

    added status codes and response types

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -13,15 +13,30 @@ namespace MagicVilla_VillaAPI.Controllers
 	public class VillaAPIController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<VillaDTO> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
 		{
-			return VillaStore.villaList;
+			return Ok(VillaStore.villaList);
 		}
 
-        [HttpGet("id")]
-        public VillaDTO GetVilla(int id)
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        //        [ProducesResponseType(200, Type = typeof(VillaDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault( u => u.Id == id );
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
     }
 }
